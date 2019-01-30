8fcc117a17a10824b0634a12a9b37dfb9416238a
Manolo Carrasco <manolo@apache.org>
2019-01-30 11:43:25 +0100

    chore(backend): do not fork twice, so as we know when spring-boot failed

--- scripts/start/backend.js
+++ scripts/start/backend.js
@@ -16,19 +16,24 @@ const exec = (cmd, args, options = {}) => {
     return new Promise((resolve, reject) => {
       const childProcess = spawn(cmd, args, options);
       // Ensure the child process is killed on shutdown
-      const killer = () => childProcess.kill();
-      process.addListener('exit', killer);
+      process.addListener('exit', childProcess.kill, {once: true});
       childProcess.on('exit', code => {
-        process.removeListener('exit', killer);
         if (code === 0) {
-          resolve(code);
+          resolve({code});
         } else {
-          reject(code);
+          reject({code});
         }
       });
     });
   } else {
-    return execFileSync(cmd, args, options);
+    return new Promise((resolve, reject) => {
+      try {
+        execFileSync(cmd, args, options);
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    })
   }
 };
 
@@ -40,16 +45,22 @@ process.on('SIGBREAK', () => process.exit(0));
 process.on('SIGHUP', () => process.exit(129));
 process.on('SIGTERM', () => process.exit(137));
 
-// Server
-const endOfOptionsIndex = process.argv.indexOf('--');
-const [chainedExecutable, ...chainedArgs] = endOfOptionsIndex > -1
-  ? process.argv.slice(endOfOptionsIndex + 1)
-  : [];
-execMaven(['compile', 'spring-boot:start', '-Dspring-boot.run.fork'], {async: true})
+execMaven(['-q', 'compile', 'spring-boot:start', '-Dspring-boot.run.fork'], )
+  .catch(() => {
+    console.log('\n🚫 \x1b[36mVaadin Connect\x1b[0m \x1b[31;1mUnable to start the backend. Check whether another instance is already running \x1b[0m 🚫\n');
+  })
   .then(() => {
-    console.log();
 
-    // Java watcher
+    // At this point spring-boot has successfully started as a daemon
+    // We need to assure that we do not leave it running on exit.
+    process.addListener('exit', () => {
+      execMaven(['-q', 'spring-boot:stop'], {stdio: 'ignore', async: true});
+    }, {once: true});
+
+    // Pass backend URL to other processes
+    process.env.BACKEND = url;
+
+    // Run the Java watcher
     if (process.argv.indexOf('--nowatch') < 0) {
       execMaven(['-q', 'fizzed-watcher:run'], {async: true})
         .catch(process.exit);
@@ -58,12 +69,14 @@ execMaven(['compile', 'spring-boot:start', '-Dspring-boot.run.fork'], {async: tr
 
     console.log(` 🌀  \x1b[36mVaadin Connect\x1b[0m Backend is running at: ${url}`);
 
-    // Pass backend URL to API browser and webpack
-    process.env.BACKEND = url;
+    // Run other processes passed through the arguments line
+    const endOfOptionsIndex = process.argv.indexOf('--');
+    const [chainedExecutable, ...chainedArgs] = endOfOptionsIndex > -1
+      ? process.argv.slice(endOfOptionsIndex + 1)
+      : [];
 
     if (chainedExecutable) {
       return exec(chainedExecutable, chainedArgs, {async: true})
         .then(process.exit);
     }
-  })
-  .catch(process.exit);
+  });
