96908f112e36f8efbbe8c6adc44e23d480e02673
Anton Platonov <platosha@gmail.com>
2019-02-19 11:29:09 +0200

    refactor: rewrite unit and e2e tests in TypeScript
    Fixes #87

--- frontend/test/utils.js
+++ frontend/test/utils.js
@@ -8,26 +8,6 @@ require('ts-node').register({
   }
 });
 
-// Enable ES modules transpilation, including dependencies
-// from `node_modules/@vaadin` under current working directory.
-const babelPackageOpts = require('../../package.json').babel;
-require('@babel/register')(Object.assign({}, babelPackageOpts, {
-  ignore: [
-    filepath => {
-      const cwd = process.cwd();
-      if (!filepath.startsWith(cwd)) {
-        return false;
-      }
-
-      filepath = filepath.slice(cwd.length);
-      const parts = filepath.split(path.sep);
-      const nodeModulesIndex = parts.indexOf('node_modules');
-      return nodeModulesIndex > -1 &&
-        parts[nodeModulesIndex + 1] !== '@vaadin';
-    }
-  ]
-}));
-
 // Provide proxyquire to enable mocking dependencies in unit tests
 intern.registerPlugin('proxyquire', () => {
   const proxyquire = require('proxyquire');
